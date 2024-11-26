f46b724cd9a3632db419b95a9d409ca2349b601b
lgaxd <lucasgrilloxd@gmail.com>
2024-11-26 18:33:24 -0300

    Subindo projeto final Instalike

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import routes from "./src/routes/postRoutes.js"
 
 const app = express();
+app.use(express.static("uploads"))
+routes(app)
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas a imersao!");
-});
---
+++ src/models/postsModel.js
@@ -0,0 +1,23 @@
+import { ObjectId } from "mongodb";
+import conectarAoBanco from "../config/dbconfig.js";
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+export async function getTodosOsPosts(){
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+};
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+};
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
+};
